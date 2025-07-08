6d3841d20d537050e164de03b9814c3206206071
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-07-08 18:26:59 -0400

    fix error route edit not exist

--- vinoprojet/resources/views/user/profile.blade.php
+++ vinoprojet/resources/views/user/profile.blade.php
@@ -11,7 +11,7 @@
         <p><strong>Email :</strong> {{ Auth::user()->email }}</p>
         <p><strong>Date d'inscription :</strong> {{ Auth::user()->created_at->format('d/m/Y') }}</p>
 
-        <a href="{{ route('profile.edit') }}" class="btn">Modifier le profil</a>
+        <a href="#" class="btn">Modifier le profil</a>
     </div>
 </div>
 @endsection
