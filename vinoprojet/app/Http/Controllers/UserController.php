743967b6723ae1a3b31605bbe87821a2efb90178
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-07-08 18:24:17 -0400

    create function user show

--- vinoprojet/app/Http/Controllers/UserController.php
+++ vinoprojet/app/Http/Controllers/UserController.php
@@ -49,9 +49,9 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show()
     {
-        //
+        return view('user.profile');
     }
 
     /**
