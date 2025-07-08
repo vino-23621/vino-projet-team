8b4778c984f7bf3c3715a9662e3e90eb09b82c9a
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-07-08 17:01:58 -0400

    add errors to form and fix method

--- vinoprojet/resources/views/user/create.blade.php
+++ vinoprojet/resources/views/user/create.blade.php
@@ -9,24 +9,29 @@
            <p>Déjà inscrit·e ?</p>
            <a href="#">Connecte-toi</a>
        </div>
-       <form action="POST">
+       <form method="POST">
         @csrf
          
         <input type="text" id="name" name="name" value="{{ old('name') }}" placeholder="Nom *">
-        <span class="form-content-error">Erreur de validation</span>
-
-        <input type="text" id="username" name="username" value="{{ old('username') }}" placeholder="Nom d’utilisateur *">
-        <span class="form-content-error">Erreur de validation</span>
+        @if ($errors->has('name'))
+        <span class="form-content-error">{{ $errors->first('name')}}</span>
+        @endif  
 
         <input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="Adresse courriel *">
-        <span class="form-content-error">Erreur de validation</span>
+        @if ($errors->has('email'))
+        <span class="form-content-error">{{ $errors->first('email')}}</span>
+        @endif 
 
         <input type="password" id="password" name="password" placeholder="Mot de passe">
-        <span class="form-content-error">Erreur de validation</span>
+        @if ($errors->has('password'))
+        <span class="form-content-error">{{ $errors->first('password')}}</span>
+        @endif 
 
-        <input type="password" id="password" name="confirmed-password" placeholder="Retaper mot de passe">
-        <span class="form-content-error">Erreur de validation</span>
-        
+        <input type="password" id="password" name="password_confirmation" placeholder="Retaper mot de passe">
+        @if ($errors->has('password_confirmation'))
+        <span class="form-content-error">{{ $errors->first('password_confirmation')}}</span>
+        @endif 
+
         <button type="submit" class="button">Créer mon compte</button>
        </form>
     </div>
