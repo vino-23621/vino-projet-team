1eab8421a9778c4f819c4a6107f8fe6874fed837
Amir-nkn <Amirhossein.niakan1@gmail.com>
2025-07-08 21:21:31 -0400

    Update User model

--- vinoprojet/app/Models/User.php
+++ vinoprojet/app/Models/User.php
@@ -12,7 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-
+    /**
+     * A user can have multiple cellars.
+     */
     public function cellars()
     {
         return $this->hasMany(Cellar::class);
@@ -48,12 +50,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    /**
-     * A user can have multiple cellars.
-     */
-    public function cellars()
-    {
-        return $this->hasMany(Cellar::class);
-    }
 }
