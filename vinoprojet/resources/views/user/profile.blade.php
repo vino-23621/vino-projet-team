a9f2a33a272a5526bbe356a3df8699808d7d573c
Amir-nkn <Amirhossein.niakan1@gmail.com>
2025-07-09 20:12:13 -0400

    Clean up profile Blade layout to align with updated CSS structure

--- vinoprojet/resources/views/user/profile.blade.php
+++ vinoprojet/resources/views/user/profile.blade.php
@@ -13,7 +13,6 @@
 
             <div class="profile-info">
                 <h3>Mes infos</h3>
-
                 <div class="profile-info-line">
                     <span class="profile-label">Nom</span>
                     <span class="profile-value">{{ Auth::user()->name }}</span>
@@ -31,10 +30,24 @@
                     <span class="profile-value">••••••••</span>
                     <a href="{{ route('user.edit', Auth::id()) }}" class="profile-edit-link">Changer</a>
                 </div>
+                <input type="checkbox" id="modalUser-toggle" class="modalUser-toggle" hidden>
 
+
+                <label for="modalUser-toggle" class="button button__danger">Supprimer le compte</label>
+                <div class="modalUser">
+                    <div class="modalUser-box">
+                        <p>Voulez-vous vraiment supprimer?</p>
+                        <div>
+                            <form action="{{ route('user.destroy', Auth::user()->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="button button__danger">Supprimer</button>
+                            </form>
+                            <label for="modalUser-toggle" class="button button__safe">Annuler</label>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 
-@endsection
+        @endsection
