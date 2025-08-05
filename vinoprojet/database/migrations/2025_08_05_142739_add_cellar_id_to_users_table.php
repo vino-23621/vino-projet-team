df3b0c7eba29e97b4191ee83f868233a0d2c4a05
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-08-05 16:06:35 -0400

    cleaning migrations database

--- vinoprojet/database/migrations/2025_08_05_142739_add_cellar_id_to_users_table.php
+++ vinoprojet/database/migrations/2025_08_05_142739_add_cellar_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('cellar_id')->after('password');
+            $table->unsignedBigInteger('cellar_id')->nullable()->after('password');
         });
     }
 
