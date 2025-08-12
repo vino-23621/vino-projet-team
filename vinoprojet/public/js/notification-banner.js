ca6770f424039e515f0b3e940bad4d5dbc44b499
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-08-12 19:52:09 -0400

    fix delai banner notification auto hide & fix validate french in auth/user controllers

--- vinoprojet/public/js/notification-banner.js
+++ vinoprojet/public/js/notification-banner.js
@@ -4,3 +4,7 @@ const notificationBanner = document.querySelector('[data-js="notification-banner
 closeBtn.addEventListener("click", () => {
     notificationBanner.style.display = "none";
 });
+
+setTimeout(() => {
+  notificationBanner.style.display = "none";
+}, 4000);
