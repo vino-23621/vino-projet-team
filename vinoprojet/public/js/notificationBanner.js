3175ec8fb4dc04d1310ec3fc9c2a4dfcfbf93378
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-08-14 15:41:34 -0400

    auth blade correction & fix bug css notification banner

--- vinoprojet/public/js/notificationBanner.js
+++ vinoprojet/public/js/notificationBanner.js
@@ -1,14 +1,10 @@
 const closeBtn = document.querySelector('[data-js="notification-close"]');
 const notificationBanner = document.querySelector('[data-js="notification-banner"]');
 
-//  setTimeout(() => {
-//    notificationBanner.classList.add('is-hidden');
-//  }, 4000);
+ setTimeout(() => {
+   notificationBanner.classList.add('is-hidden');
+ }, 4000);
 
 closeBtn.addEventListener("click", () => {
     notificationBanner.classList.add('is-hidden');
 });
-
-// setTimeout(() => {
-//   notificationBanner.style.display = "none";
-// }, 4000);
