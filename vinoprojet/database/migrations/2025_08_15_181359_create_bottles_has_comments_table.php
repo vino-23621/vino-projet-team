d05758b5019f8b225e811bba86a81a8356e59091
Juan Hernandez <juahz@icloud.com>
2025-08-15 14:51:41 -0400

    Adding migration for new tables comments and bottle_has_comments also, modifying table bottle for getting more properties, finally,  updating bootles.js for getting all data

---
+++ vinoprojet/database/migrations/2025_08_15_181359_create_bottles_has_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bottles_has_comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bottle_id');
+            $table->unsignedBigInteger('comment_id');
+            $table->longText('comment');
+            $table->timestamps();
+
+            $table->foreign('bottle_id')->references('id')->on('bottles')->onDelete('cascade');
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bottles_has_comments');
+    }
+};
